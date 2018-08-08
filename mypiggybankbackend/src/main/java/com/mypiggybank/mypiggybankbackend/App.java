71d1eea61183564ff84a64836c2bae4e2b599878
ugochukwu <ugochukwuomeje1@gmail.com>
2018-08-08 18:30:36 +0200

    Piggy bank setup completed successfully

---
+++ mypiggybankbackend/src/main/java/com/mypiggybank/mypiggybankbackend/App.java
@@ -0,0 +1,13 @@
+package com.mypiggybank.mypiggybankbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
